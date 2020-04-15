7a0c3269b128e2eaf07cbe67ac996372bdf5308e
Dwi Noviandaru <dnoviandaru@gmail.com>
2020-04-15 14:03:48 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
+import {BrowserRouter, Switch, Route} from "react-router-dom";
+
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Header from "./Header";
+import Blog from "./Blog";
+import BlogDetail from './BlogDetail';
+
+//install bootstrap dulu
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// Ini merupakan functional component
+// https://getbootstrap.com/docs/4.4/examples/navbar-static/
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <div className="App">
+        <Header />
+        <Switch>
+          <Route exact path="/about">
+            About
+          </Route>
+          <Route exact path="/blog/:articleId" component={BlogDetail} />
+          <Route exact path="/blog">
+            <Blog />
+          </Route>
+          <Route exact path="/">
+            Home
+          </Route>
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
